1aafe2534aa1bfaff4a9bce24c4880007f065047
Igor Lopushko <lopushko.igor@gmail.com>
2022-10-26 23:39:00 +0300

    Fix default values of the config

--- app/config/config.go
+++ app/config/config.go
@@ -16,16 +16,16 @@ var (
 // Represents app configuration type.
 type Config struct {
 	Board    BoardConfig
-	LogLevel string `env:"LOG_LEVEL" env-default:"debug"`
+	LogLevel string `env:"LOG_LEVEL" envDefault:"debug"`
 }
 
 // Represents game board configuration type.
 type BoardConfig struct {
-	Width           int `env:"BOARD_WIDTH" env-default:"10"`
-	WidthMax        int `env:"BOARD_WIDTH_MAX" env-default:"10"`
-	Height          int `env:"BOARD_HEIGHT" env-default:"10"`
-	HeightMax       int `env:"BOARD_HEIGHT_MAX" env-default:"10"`
-	BlackHolesCount int `env:"BOARD_BLACK_HOLES_COUNT" env-default:"5"`
+	Width           int `env:"BOARD_WIDTH" envDefault:"8"`
+	WidthMax        int `env:"BOARD_WIDTH_MAX" envDefault:"10"`
+	Height          int `env:"BOARD_HEIGHT" envDefault:"10"`
+	HeightMax       int `env:"BOARD_HEIGHT_MAX" envDefault:"8"`
+	BlackHolesCount int `env:"BOARD_BLACK_HOLES_COUNT" envDefault:"10"`
 }
 
 // Loads configuration from the config file into environment variables.
